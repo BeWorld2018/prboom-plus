b6c2795e5f284604abdeb924ac48d40778ff179a
BeWorld <36823759+BeWorld2018@users.noreply.github.com>
2021-01-30 07:29:20 +0100

    MORPHOS: add config.h and makefile.mos
    - disable Shaders
    - disable NetWork support

---
+++ prboom2/src/config.h
@@ -0,0 +1,49 @@
+#define PACKAGE_NAME "PrBoom-Plus"
+#define PACKAGE_TARNAME "prboom-plus"
+#define PACKAGE_VERSION "2.5.1.7"
+#define PACKAGE_STRING "PrBoom-Plus 2.5.1.7"
+
+#define DOOMWADDIR ""
+
+#define WORDS_BIGENDIAN
+
+#define HAVE_GETOPT
+/* #undef HAVE_MMAP */
+/* #undef HAVE_SCHED_SETAFFINITY */
+#define HAVE_USLEEP
+/*#define HAVE_STRSIGNAL*/
+/* #undef HAVE_MKSTEMP */
+
+#define HAVE_SYS_WAIT_H
+#define HAVE_UNISTD_H
+/* #undef HAVE_ASM_BYTEORDER_H */
+
+#define HAVE_SDL_JOYSTICKGETAXIS
+#define HAVE_LIBSDL2_IMAGE
+#define HAVE_LIBSDL2_MIXER
+//#define HAVE_NET
+//#define USE_SDL_NET
+
+/* #undef HAVE_LIBPCREPOSIX */
+#define HAVE_LIBZ
+/*#define HAVE_LIBMAD*/
+/* #undef HAVE_LIBFLUIDSYNTH */
+/* #undef HAVE_LIBDUMB */
+/*#define HAVE_LIBVORBISFILE*/
+/* #undef HAVE_LIBPORTMIDI */
+
+#define SIMPLECHECKS
+#define ZONEIDCHECK
+
+//#define RANGECHECK 
+//#define INSTRUMENTED 
+//#define TIMEDIAG 
+//#define HEAPCHECK 
+//#define HEAPDUMP 
+
+#define GL_DOOM
+//#define USE_SHADERS
+#define USE_FBO_TECHNIQUE
+#define USE_GLU_IMAGESCALE
+#define USE_GLU_MIPMAP
+#define USE_GLU_TESS
